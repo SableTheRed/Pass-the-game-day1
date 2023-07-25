1306aac7ffcacaf4e546b39ad8754f52b345f270
SableTheRed <113140845+SableTheRed@users.noreply.github.com>
2023-07-25 20:53:14 +0100

    There's an enemy prefab who follows you now
    A very basic triangle will move towards the player. Looking to implement a system to grab and move objects inside the world, but that's incomplete for now

--- Assets/Scripts/GrabController.cs
+++ Assets/Scripts/GrabController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WASD_MovementScript : MonoBehaviour
+public class GrabController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
